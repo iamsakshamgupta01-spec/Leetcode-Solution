22c521c4560aaddb3015c87b31b22d2651b5943b
iamsakshamgupta01-spec <iamsakshamgupta01@gmail.com>
2025-11-14 14:22:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.76%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeElement(self, nums, val):
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            if nums[left] == val:
+                nums[left] = nums[right]
+                right -= 1
+            else:
+                left += 1
+        
+        return left
